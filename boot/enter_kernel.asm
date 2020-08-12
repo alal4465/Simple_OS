2d2c0273dbd215f6dd8927b2b1ce62c7d717cbe0
alal4465 <alonbenti@gmail.com>
2020-08-12 18:54:06 +0300

    booting successfully

--- boot/enter_kernel.asm
+++ boot/enter_kernel.asm
@@ -1,4 +1,6 @@
 [bits 32]
-[extern main] ; Define calling point. Must have same name as kernel.c 'main' function
-call main ; Calls the C function. The linker will know where it is placed in memory
+
+[extern main]
+call main
+
 jmp $
