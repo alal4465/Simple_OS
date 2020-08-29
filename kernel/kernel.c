20b7202f88399eb0e4646280b467b2959c0fc4b2
alal4465 <60041914+alal4465@users.noreply.github.com>
2020-08-29 17:09:44 +0300

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,9 @@
 #include "../drivers/screen.h"
 #include "idt.h"
+#include "memory.h"
+#include "kheap.h"
+#include "libc.h"
+
 void main(){
 
 	screen_clear();
@@ -12,5 +16,5 @@ void main(){
 	__asm__("int $0x00");
 	__asm__("int $0x80");
 
-
+	init_paging();
 }
